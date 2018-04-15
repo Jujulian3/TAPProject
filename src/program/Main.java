ccc7d1cb5bd3491ca7201c05134ea004e54d2962
Juliane Cristina <jujulian3@gmail.com>
2018-04-15 13:39:36 -0300

    TelaGrafica Tentativa harded code

--- src/program/Main.java
+++ src/program/Main.java
@@ -1,13 +1,20 @@
 package program;
 
 import entities.*;
+import graphics.ProgramGUI;
 
 public class Main {
 	public static void main(String[] args) {
 
-		ThreadDriver d1 = new ThreadDriver("João");
-		ThreadDriver d2 = new ThreadDriver("Pedro");
-		ThreadDriver d3 = new ThreadDriver("Diego");
-		ThreadDriver d4 = new ThreadDriver("Antônio");
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new ProgramGUI().setVisible(true);
+			}
+		});
+
+		//ThreadDriver d1 = new ThreadDriver("João");
+		//ThreadDriver d2 = new ThreadDriver("Pedro");
+		//ThreadDriver d3 = new ThreadDriver("Diego");
+		//ThreadDriver d4 = new ThreadDriver("Antônio");
 	}
 }
